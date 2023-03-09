bb27db9746d5d7e07574ed58b9fd9083c2529e2d
vitorbeltrao <vitorbeltraoo@hotmail.com>
2023-03-09 12:53:52 -0300

    🎨 Improve the system structure

--- ml_api.py
+++ ml_api.py
@@ -6,12 +6,13 @@ this file is for creating our inference api with fastapi
 
 # Import necessary packages
 import json
+import joblib
 import logging
-import mlflow
+# import mlflow
 import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
-import wandb
+# import wandb
 
 # basic logs config
 logging.basicConfig(
@@ -41,21 +42,44 @@ class ModelInput(BaseModel):
     hours_per_week: int
     native_country: str
 
+    class Config:
+        schema_extra = {
+                        "example": {
+                                    "age": 50,
+                                    "workclass": " Private", 
+                                    "fnlwgt": 234721,
+                                    "education": " Doctorate",
+                                    "education_num": 16,
+                                    "marital_status": " Separated",
+                                    "occupation": " Exec-managerial",
+                                    "relationship": " Not-in-family",
+                                    "race": " Black",
+                                    "sex": " Female",
+                                    "capital_gain": 0,
+                                    "capital_loss": 0,
+                                    "hours_per_week": 50,
+                                    "native_country": " United-States"
+                                    }
+                        }
 
-# loading the saved model
-# start a new run at wandb
-run = wandb.init(
-    project='census-income-forecast',
-    entity='vitorabdo',
-    job_type='get_mlflow_model')
+# # loading the saved model
+# # start a new run at wandb
+# run = wandb.init(
+#     project='census-income-forecast',
+#     entity='vitorabdo',
+#     job_type='get_mlflow_model')
+
+# # download mlflow model
+# model_local_path = run.use_artifact(
+#     'vitorabdo/census-income-forecast/final_model_pipe:prod',
+#     type='pickle').download()
+# sk_pipe = mlflow.sklearn.load_model(model_local_path)
+# wandb.finish()
+# logger.info('Downloaded prod mlflow model: SUCCESS')
 
 # download mlflow model
-model_local_path = run.use_artifact(
-    'vitorabdo/census-income-forecast/final_model_pipe:prod',
-    type='pickle').download()
-sk_pipe = mlflow.sklearn.load_model(model_local_path)
-wandb.finish()
-logger.info('Downloaded prod mlflow model: SUCCESS')
+model_local_path = 'model.pkl'
+sk_pipe = joblib.load(model_local_path)
 
 
 @app.get('/')
--- post_to_api.py
+++ post_to_api.py
@@ -11,20 +11,20 @@ url = "https://census-income-forecast.herokuapp.com"
 
 # explicit the sample to perform inference on
 sample =  {
-    'age': 50,
-    'workclass': ' Private', 
-    'fnlgt': 234721,
-    'education': ' Doctorate',
-    'education_num': 16,
-    'marital_status': ' Separated',
-    'occupation': ' Exec-managerial',
-    'relationship': ' Not-in-family',
-    'race': ' Black',
-    'sex': ' Female',
-    'capital_gain': 0,
-    'capital_loss': 0,
-    'hours_per_week': 50,
-    'native_country': ' United-States'
+    "age": 50,
+    "workclass": " Private", 
+    "fnlwgt": 234721,
+    "education": " Doctorate",
+    "education_num": 16,
+    "marital_status": " Separated",
+    "occupation": " Exec-managerial",
+    "relationship": " Not-in-family",
+    "race": " Black",
+    "sex": " Female",
+    "capital_gain": 0,
+    "capital_loss": 0,
+    "hours_per_week": 50,
+    "native_country": " United-States"
 }
 
 data = json.dumps(sample)
